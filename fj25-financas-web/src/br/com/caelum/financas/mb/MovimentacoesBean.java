0b3496ddf7f82bc1971b987f9f32187c1b74873e
Tiago Tiede <geschopf@gmail.com>
2018-05-12 09:45:28 -0300

    Init web project fj 25

---
+++ fj25-financas-web/src/br/com/caelum/financas/mb/MovimentacoesBean.java
@@ -0,0 +1,82 @@
+package br.com.caelum.financas.mb;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.caelum.financas.dao.MovimentacaoDao;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+
+@Named
+@ViewScoped
+public class MovimentacoesBean implements Serializable {
+
+	@Inject
+	private MovimentacaoDao movimentacaoDao;
+	private static final long serialVersionUID = 1L;
+
+	private List<Movimentacao> movimentacoes;
+	private Movimentacao movimentacao = new Movimentacao();
+	private Integer contaId;
+	private Integer categoriaId;
+
+	public void grava() {
+		movimentacaoDao.adiciona(movimentacao);
+		this.movimentacoes = movimentacaoDao.lista();
+		limpaFormularioDoJSF();
+	}
+
+	public void remove() {
+		movimentacaoDao.remove(this.movimentacao);
+		this.movimentacoes = movimentacaoDao.lista();
+		limpaFormularioDoJSF();
+	}
+
+	public List<Movimentacao> getMovimentacoes() {
+		return movimentacoes;
+	}
+
+	public Movimentacao getMovimentacao() {
+		if (movimentacao.getData() == null) {
+			movimentacao.setData(LocalDateTime.now());
+		}
+		return movimentacao;
+	}
+
+	public void setMovimentacao(Movimentacao movimentacao) {
+		this.movimentacao = movimentacao;
+	}
+
+	public Integer getContaId() {
+		return contaId;
+	}
+
+	public void setContaId(Integer contaId) {
+		this.contaId = contaId;
+	}
+
+	public Integer getCategoriaId() {
+		return categoriaId;
+	}
+
+	public void setCategoriaId(Integer categoriaId) {
+		this.categoriaId = categoriaId;
+	}
+
+	/**
+	 * Esse metodo apenas limpa o formulario da forma com que o JSF espera.
+	 * Invoque-o no momento manager que precisar do formulario vazio.
+	 */
+	private void limpaFormularioDoJSF() {
+		this.movimentacao = new Movimentacao();
+	}
+
+	public TipoMovimentacao[] getTiposDeMovimentacao() {
+		return TipoMovimentacao.values();
+	}
+}
---
+++ fj25-financas-web/src/br/com/caelum/financas/service/Agendador.java
@@ -0,0 +1,37 @@
+package br.com.caelum.financas.service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Stateless;
+
+@Stateless
+//@Singleton
+//@AccessTimeout(unit	=	TimeUnit.SECONDS,	value	=	5)
+
+public class Agendador {
+
+	private static int totalCriado;
+
+	@PostConstruct
+	void posConstrucao() {
+		System.out.println("criando	agendador");
+		totalCriado++;
+	}
+
+	@PreDestroy
+	void preDestruicao() {
+		System.out.println("destruindo	agendador");
+	}
+
+	public void executa() {
+		System.out.printf("%d instancias criadas %n", totalCriado);
+
+		// simulando demora de 4s na execucao
+		try {
+			System.out.printf("Executando %s %n", this);
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+		}
+	}
+
+}
