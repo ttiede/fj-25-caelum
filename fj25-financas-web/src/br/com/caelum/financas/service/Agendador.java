6d52071c7dd51c52ff1bcd829654d9b31b9e64a8
Tiago Tiede <geschopf@gmail.com>
2018-05-12 10:22:55 -0300

    Scheduler

--- fj25-financas-web/src/br/com/caelum/financas/service/Agendador.java
+++ fj25-financas-web/src/br/com/caelum/financas/service/Agendador.java
@@ -2,20 +2,49 @@ package br.com.caelum.financas.service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.ejb.Stateless;
+import javax.annotation.Resource;
+import javax.ejb.Schedule;
+import javax.ejb.ScheduleExpression;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
 
-@Stateless
-//@Singleton
-//@AccessTimeout(unit	=	TimeUnit.SECONDS,	value	=	5)
-
+//@Stateless
+@Singleton
+// @AccessTimeout(unit = TimeUnit.SECONDS, value = 5)
+@Startup
 public class Agendador {
 
 	private static int totalCriado;
+	@Resource
+	private TimerService timerService;
 
 	@PostConstruct
 	void posConstrucao() {
 		System.out.println("criando	agendador");
 		totalCriado++;
+	}
+
+	public void agenda(String expressaoMinutos, String expressaoSegundos) {
+		ScheduleExpression expression = new ScheduleExpression();
+		expression.hour("*");
+		expression.minute(expressaoMinutos);
+		expression.second(expressaoSegundos);
+		TimerConfig config = new TimerConfig();
+		config.setInfo(expression.toString());
+		config.setPersistent(false);
+		this.timerService.createCalendarTimer(expression, config);
+		System.out.println("Agendamento:	" + expression);
+	}
+
+	@Timeout
+	public void verificacaoPeriodicaSeHaNovasContas(Timer timer) {
+		System.out.println(timer.getInfo());
+		// aqui poderiamos acessar o banco de dados
+		// com JPA para verificar as contas periodicamente
 	}
 
 	@PreDestroy
@@ -34,4 +63,8 @@ public class Agendador {
 		}
 	}
 
+	@Schedule(hour = "9", minute = "0", second = "0", dayOfWeek = "Mon", persistent = false)
+	public void enviaEmailCadaMinutoComInformacoesDasUltimasMovimentacoes() {
+		System.out.println("enviando	email	semanal");
+	}
 }
